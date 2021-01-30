c538a9035ebb7b47e546f9f9a42a9a867b4c944c
balrom1981 <balrom@bk.ru>
2021-01-30 17:06:03 +0300

    Radio changed

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -34,56 +34,6 @@ public class Radio {
         this.currentVolume = currentVolume;
 
     }
-//
-//    public int getMaxStation() {
-//        return maxStation;
-//    }
-//
-//    public void setMaxStation(int maxStation) {
-//        this.maxStation = maxStation;
-//    }
-//
-//    public int getMinStation() {
-//        return minStation;
-//    }
-//
-//    public void setMinStation(int minStation) {
-//        this.minStation = minStation;
-//    }
-//
-//    public int getMaxVolume() {
-//        return maxVolume;
-//    }
-//
-//    public void setMaxVolume(int maxVolume) {
-//        this.maxVolume = maxVolume;
-//    }
-//
-//    public int getMinVolume() {
-//        return minVolume;
-//    }
-//
-//    public void setMinVolume(int minVolume) {
-//        this.minVolume = minVolume;
-//    }
-//
-//    public long getCurrentVolume() {
-//        return currentVolume;
-//    }
-//
-//    public void setCurrentVolume(int currentVolume) {
-//        this.currentVolume = currentVolume;
-//
-//    }
-//
-//    public int getCurrentStation() {
-//        return currentStation;
-//    }
-//
-//    public void setCurrentStation(int currentStation) {
-//
-//        this.currentStation = currentStation;
-//    }
 
     public void nextStation() {
         if (currentStation < maxStation) {
