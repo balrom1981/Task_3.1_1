594a9c4e0d9f0db0ca21fb866af5eb76ed3d3aa1
balrom1981 <balrom@bk.ru>
2021-01-30 16:03:42 +0300

    Lombok added

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,5 +1,13 @@
 package ru.netology.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+
+
 public class Radio {
     private int maxStation = 10;
     private int minStation = 0;
@@ -26,56 +34,56 @@ public class Radio {
         this.currentVolume = currentVolume;
 
     }
-
-    public int getMaxStation() {
-        return maxStation;
-    }
-
-    public void setMaxStation(int maxStation) {
-        this.maxStation = maxStation;
-    }
-
-    public int getMinStation() {
-        return minStation;
-    }
-
-    public void setMinStation(int minStation) {
-        this.minStation = minStation;
-    }
-
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
-
-    public long getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        this.currentVolume = currentVolume;
-
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
-    public void setCurrentStation(int currentStation) {
-
-        this.currentStation = currentStation;
-    }
+//
+//    public int getMaxStation() {
+//        return maxStation;
+//    }
+//
+//    public void setMaxStation(int maxStation) {
+//        this.maxStation = maxStation;
+//    }
+//
+//    public int getMinStation() {
+//        return minStation;
+//    }
+//
+//    public void setMinStation(int minStation) {
+//        this.minStation = minStation;
+//    }
+//
+//    public int getMaxVolume() {
+//        return maxVolume;
+//    }
+//
+//    public void setMaxVolume(int maxVolume) {
+//        this.maxVolume = maxVolume;
+//    }
+//
+//    public int getMinVolume() {
+//        return minVolume;
+//    }
+//
+//    public void setMinVolume(int minVolume) {
+//        this.minVolume = minVolume;
+//    }
+//
+//    public long getCurrentVolume() {
+//        return currentVolume;
+//    }
+//
+//    public void setCurrentVolume(int currentVolume) {
+//        this.currentVolume = currentVolume;
+//
+//    }
+//
+//    public int getCurrentStation() {
+//        return currentStation;
+//    }
+//
+//    public void setCurrentStation(int currentStation) {
+//
+//        this.currentStation = currentStation;
+//    }
 
     public void nextStation() {
         if (currentStation < maxStation) {
